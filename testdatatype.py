32c98e009e9d53e1593c35163468228b0a78ed3c
ouksreylin <ouk.sreylin12@gmail.com>
2024-07-20 11:06:26 +0700

    first commit

---
+++ testdatatype.py
@@ -0,0 +1,7 @@
+# test datatype
+str = "Hello World"
+print(str)  # print all
+print(str[2:5]) # print from index 2 to 5
+print(str[2:])  # print from index 2 to all index after 2
+print(str*3)    # print all 3times
+print(str+"From BBU")   # print all and add string
