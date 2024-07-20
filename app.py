96e50f0a724e1ba60d79029eb220790135ca8cb8
ouksreylin <ouk.sreylin12@gmail.com>
2024-07-20 10:04:41 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# print("Hello Student"+" From BBU"+str(5))
+# print("Hi")
+#
+# print("Single 'quote' inside double quote")
+# print('Single "quote" inside single quote')
+# print("Single \'quote\' and \"double\" quote \ninside quote")
+#
+# # test variable
+# name=input("What is your name? ")
+# print("Welcome "+name)
+#
+# # test01
+# print("==Welcome Amount Cal==")
+# qty1=int(input("Qty1 = "))
+# qty2=int(input("Qty2 = "))
+# price=float(input("Price = "))
+# totalqty=qty1+qty2
+# print("Total Qty = "+str(totalqty))
+# amount=totalqty*price
+# print("Amount = "+str(amount))
+
+
+# # test02
+# a=input()
+# b=input()
+#
+# c=a
+# a=b
+# b=c
+#
+#
+# print("a : "+a)
+# print("b : "+b)
+
+
+
+# test03
+print("Welcome to the band name generator")
+city=input("Which city did you grow up in ? \n")
+pet=input("What is the name of a pet ? \n")
+print("Your band name could be "+city+" "+pet)
